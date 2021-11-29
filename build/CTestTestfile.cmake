dffba8ff9d3a752c72abdb865f96e81bf434b957
somikdhar9897 <45973106+somikdhar729@users.noreply.github.com>
2021-11-29 20:54:03 +0530

    Create CTestTestfile.cmake

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/ros_assignment/src
+# Build directory: /home/user/ros_assignment/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("solutions")
